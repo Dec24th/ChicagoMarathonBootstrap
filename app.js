44fe205bad55d99fb376ee6bccf56080e9ca710e
Allan Acosta <acosta.gov@gmail.com>
2022-02-09 19:56:02 -0500

    Edited the entire page, splash page as front page. No routers. I'll do that tomorrow. Added border, plain looking page

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const datas = require('./data/data.json');
+const coaches = require('./data/coaches.json');
 
 const PORT = process.env.PORT || 5001
 
 const app = express()
 
+app.use(express.static(__dirname + '/public'));
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
-    res.render('index')
+    res.render('index', { title: "NYC MARATHON 2022" , datas} )
+})
+
+app.get('/register', (req, res) => {
+    res.render('register', { title22: "Registration", datas } )
 })
 
 app.listen(PORT, () => {
     debug(`Listening on port ${PORT}`)
+    console.log(`listening on port ${PORT}`)
 })
