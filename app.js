80fc12892284fe63b64dbea775eda212618117a8
Allan Acosta <acosta.gov@gmail.com>
2022-02-10 17:23:23 -0500

    Added more to register.ejs. Applied CSS Grid System to register.ejs. Learned from PluralSight

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const datas = require('./data/data.json');
 const coaches = require('./data/coaches.json');
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 5001
 
@@ -14,11 +15,15 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
-    res.render('index', { title: "NYC MARATHON 2022" , datas} )
+    res.render('index', { title: "Homepage | NYC MARATHON 2022" , datas} )
 })
 
 app.get('/register', (req, res) => {
-    res.render('register', { title22: "Registration", datas } )
+    res.render('register', { title22: "Registration | NYC Marathon 2022", datas } )
+})
+
+app.get('/coaching', (req, res) => {
+    res.render('coaching', {coaches})
 })
 
 app.listen(PORT, () => {
